dc3eb5aab33206d32fd1e6315bf8a9e4d4fc2b68
Furkan Cetin <furkan.ceetin1@yandex.com>
2018-04-05 23:41:43 +0300

    Back Propagation Added

--- neural_network_implementation/NeuralNet.py
+++ neural_network_implementation/NeuralNet.py
@@ -1,74 +1,140 @@
 import numpy as np
 class NeuralNet:
 
-    def __init__(self, input_size, hidden_size, num_classes, reg, lr):
+    def __init__(self, input_size, hidden_size, num_classes, reg, learning_rate):
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.num_classes = num_classes
         self.reg = reg
-        self.lr = lr
+        self.learning_rate = learning_rate
 
-    def initializeWeights(self, mu=0):
+    def initializeWeights(self, input_size, hidden_size, num_classes):
         # Initializing random weights w0from sklearn.datasets import fetch_mldata
         #there are hidden_size*input_size number of weights in the first layer
         # Initializing random weights w1
         #there are hidden_size*num_classes number of weights in the first layer
-        w0 = np.random.rand(hidden_size, input_size) * 0.001
-        b0 = np.zeros(shape=(hidden_size, 1))
-        w1 = np.random.rand(num_classes, hidden_size) * 0.001
-        b0 = np.zeros(shape=(num_classes, 1))
-        parameters = {"w0":w0,
-                      "w1":w1,
-                      "b0":b0,
-                      "b1":b1}
+       
+        w1 = np.random.rand(hidden_size, input_size) * 0.001
+        b1 = np.zeros(shape=(hidden_size, 1))
+        w2 = np.random.rand(num_classes, hidden_size) * 0.001
+        b2 = np.zeros(shape=(num_classes, 1))
+        parameters = {"w1":w1,
+                      "w2":w2,
+                      "b1":b1,
+                      "b2":b2}
         return parameters
     
-    def calculateL2(self, w0, w1):
+    def calculateL2(self, w1, w2):
         # L2 Regularization loss
-        L2 = np.sum(np.square(wo)) + np.sum(np.square(w1))
+        L2 = np.sum(np.square(w1)) + np.sum(np.square(w2))
+        return L2
         
     def relu(self, neurons):
         # determines the activations of a ReLU when input is given
-        activation_neurons = np.maximum(neurons, 0)
+        return np.maximum(neurons, 0)
+
+    def relu_derivative(self, x):
+        #calculates the derivate of relu activation
+        return np.multiply(1*(x > 0),x)
         
-    def softmax(self, neurons, y):
+    def softmax(self, neurons):
          # computes normalized probabilities and loss when labels(y) are given
         class_probs = np.exp(neurons) / np.sum(np.exp(neurons))
-        return class_probs, loss
         
-    def forwardPass(self, X, w0, w1, b0, b1, y=None, training=True):
-        #forward propagation and loss computation
-        z1 = np.dot(w0, X) + b0
-        a1 = relu(z1)
-        z2 = np.dot(w1, a1) + b1
-        class_probs, loss = self.softmax(z2, y)
-        cache = {"z1":z1,
-                 "a1":a1,
-                 "z2":z1,
-                 "class_probs":class_probs
-                }
-        return class_probs, cache
-    
-    def computeCost(self, class_probs, Y_train):
-        cost = -1 * np.sum(np.log(np.multiply(class_probs, Y_train)))
-        return cost
-    
-    def predict(self, X):
-        # Given some data X, predict the class per each sample
-        class_probs, cache = self.forwardPass(X, parameters)
-        #convert class probabilities to 0 or 1 by threshold
-        class_probs = (class_probs > threshold)
         return class_probs
         
-    def backProp(self, li, labels):    
+    def forwardPass(self, X, parameters):
+        #forward propagation and loss computation
+        w1 = parameters['w1']
+        b1 = parameters['b1']
+        w2 = parameters['w2']
+        b2 = parameters['b2']
+        
+        z1 = np.dot(w1, X) + b1
+        a1 = self.relu(z1)
+        z2 = np.dot(w2, a1) + b2
+        class_probs = self.softmax(z2)
+        cache = {"z1":z1,
+                 "a1":a1,
+                 "z2":z2,
+                 "a2":class_probs
+                }
+        
+        return class_probs, cache
+    
+    def computeCost(self, class_probs, Y):
+        m = Y.shape[1]  
+        cost = -1 * np.sum(np.multiply(np.log(class_probs), Y)) / m
+        
+        return cost
+    
+    def predict(self, X, w1, w2, b1, b2, threshold):
+        # Given some data X, predict the class per each sample
+        class_probs, cache = self.forwardPass(X, w1, w2, b1, b2)
+        #convert class probabilities to 0 or 1 by threshold
+        class_probs = (class_probs > threshold)
+       
+        return class_probs
+        
+    def backProp(self, parameters, cache, X, Y):    
         #backpropagate the loss
-        #compute weight updates
-        return dw0, dw1, db0, db1
-
+        m = X.shape[1]
+        a1 = cache['a1']
+        a2 = cache['a2']
+        w2 = parameters['w2']
+        
+        dz2 = a2 - Y
+        dw2 = (1 / m) * np.dot(dz2, a1.T)
+        db2 = (1 / m) * np.sum(dz2, axis=1, keepdims=True)
+        dz1 = np.multiply(np.dot(w2.T, dz2), self.relu_derivative(a1))
+        dw1 = (1 / m) * np.dot(dz1, X.T)
+        db1 = (1 / m) * np.sum(dz1, axis=1, keepdims=True)
+        
+        grads = {"dw1": dw1,
+                 "db1": db1,
+                 "dw2": dw2,
+                 "db2": db2} 
+        
+        return grads
+    
+    def update_parameters(self, parameters, grads, learning_rate):
+        # retrieve each parameters that should be optimized
+        w1 = parameters['w1']
+        b1 = parameters['b1']
+        w2 = parameters['w2']
+        b2 = parameters['b2']
+        # retrieve each gradients to updatee the parameters
+        dw1 = grads['dw1']
+        db1 = grads['db1']
+        dw2 = grads['dw2']
+        db2 = grads['db2']
+        #update each parameter
+        w1 = w1 - learning_rate * dw1
+        b1 = b1 - learning_rate * db1
+        w2 = w2 - learning_rate * dw2
+        b2 = b2 - learning_rate * db2
+        #save updated parameters to dictionary
+        parameters = {"w1": w1,
+              "b1": b1,
+              "w2": w2,
+              "b2": b2}
+        return parameters
+    
     def train(self, X_train, Y_train, num_epoch, batch_size):
+        parameters = self.initializeWeights(self.input_size, self.hidden_size, self.num_classes)
+        m = X_train.shape[1]
+        
         for j in range(num_epoch):
-            for i in range(num_batch):
-            #forwardPass
-            #backProp
-            #weightUpdate
+            for i in range(int(m/batch_size)):
+                #forwardPass
+                class_probs, cache = self.forwardPass(X_train[:,(i*batch_size)+1:(i+1)*batch_size],parameters)
+                #computeCost
+                cost = self.computeCost(class_probs, Y_train[:,(i*batch_size)+1:(i+1)*batch_size])
+                #backProp
+                grads = self.backProp(parameters, cache, X_train[:,(i*batch_size)+1:(i+1)*batch_size], Y_train[:,(i*batch_size)+1:(i+1)*batch_size])
+                #weightUpdate
+                parameters = self.update_parameters(parameters, grads, self.learning_rate)
+            errorbyepoch = cost
+            print(class_probs.shape, Y_train[:,(i*batch_size)+1:(i+1)*batch_size].shape)
+            
         return errorbyepoch
--- neural_network_implementation/main.py
+++ neural_network_implementation/main.py
@@ -6,23 +6,23 @@ import sklearn.preprocessing
 from NeuralNet import NeuralNet
 
 def readData(mode="training"):
-	# Reading Data
-    	foldername = os.path.join(os.path.realpath(''), 'mnistdata/data')
-    
-    	mndata = mnist.MNIST(foldername)
-    	if mode == "training":
-    		images, labels = mndata.load_training()
-    	elif mode == "test":
-    		images, labels = mndata.load_testing()
-    	else:
-    		print (" Data not read.")
-    		images, labels = None
-    	print ("MNIST handwritten digit data is read.")
-    	label_binarizer = sklearn.preprocessing.LabelBinarizer()
-    	label_binarizer.fit(range(max(labels)+1))
-    	labels = label_binarizer.transform(np.array(labels)).T
-    	return np.array(images), np.array(labels)
+    # Reading Data
+    foldername = os.path.join(os.path.realpath(''), 'mnistdata/data')
 
+    mndata = mnist.MNIST(foldername)
+    if mode == "training":
+        images, labels = mndata.load_training()
+    elif mode == "test":
+        images, labels = mndata.load_testing()
+    else:
+        print (" Data not read.")
+        images, labels = None
+    print ("MNIST handwritten digit data is read.")
+    label_binarizer = sklearn.preprocessing.LabelBinarizer()
+    label_binarizer.fit(range(max(labels)+1))
+    labels = label_binarizer.transform(np.array(labels)).T
+ 
+    return np.array(images).T, np.array(labels)
 
 X_train, y_train = readData(mode="training")
 X_test, y_test = readData(mode="test")
@@ -44,20 +44,28 @@ net = NeuralNet(input_size, hidden_size, num_classes, reg, lr)
 # stats keeps the amount of error at each epoch
 errorbyepoch = net.train(X_train, y_train, num_epoch, batch_size)
 # Predict on the test
-train_acc = (net.predict(X_train) == y_train).mean()
-print ('Train accuracy: ', train_acc)
-test_acc = (net.predict(X_test) == y_test).mean()
-print ('Test accuracy: ', test_acc)
+
+
+#train_acc = (net.predict(X_train) == y_train).mean()
+#print ('Train accuracy: ', train_acc)
+#test_acc = (net.predict(X_test) == y_test).mean()
+#print ('Test accuracy: ', test_acc)
+
+
 
 # Plot the points using matplotlib
-x = np.arange(0, num_epoch, 1)
-plt.plot(x, errorbyepoch)
+
+
+#x = np.arange(0, num_epoch, 1)
+#plt.plot(x, errorbyepoch)
+
+
  
 #lri=10**np.random.uniform(-1,-5,10)
 #regi=10**np.random.uniform(-1,-5,10)
 #trials = list(range(5)) 
 #for count in trials:
-#	net = NeuralNet(input_size, hidden_size, num_classes, regi[count], lri[count])
-#	errorbyepoch = net.train(X_train, y_train, num_epoch, batch_size)
-#	test_acc = (net.predict(X_test) == y_test).mean()
-#	print ('Test accuracy: ', count, ' ', test_acc)
+#    net = NeuralNet(input_size, hidden_size, num_classes, regi[count], lri[count])
+#    errorbyepoch = net.train(X_train, y_train, num_epoch, batch_size)
+#    test_acc = (net.predict(X_test) == y_test).mean()
+#    print ('Test accuracy: ', count, ' ', test_acc)
